0049c850f6b4f4f67efedacefa64b25b49e6694c
manjari jain <manjarijain98@gmail.com>
2021-08-28 13:19:01 +0530

    separated code files

--- src/App.js
+++ src/App.js
@@ -1,49 +1,8 @@
-// Color Theme
-const colors = {
-  available: 'lightgray',
-  used: 'lightgreen',
-  wrong: 'lightcoral',
-  candidate: 'deepskyblue',
-};
-
-// Math science
-const utils = {
-  // Sum an array
-  sum: arr => arr.reduce((acc, curr) => acc + curr, 0),
-
-  // create an array of numbers between min and max (edges included)
-  range: (min, max) => Array.from({ length: max - min + 1 }, (_, i) => min + i),
-
-  // pick a random number between min and max (edges included)
-  random: (min, max) => min + Math.floor(Math.random() * (max - min + 1)),
-
-  // Given an array of numbers and a max...
-  // Pick a random sum (< max) from the set of all available sums in arr
-  randomSumIn: (arr, max) => {
-    const sets = [[]];
-    const sums = [];
-    for (let i = 0; i < arr.length; i++) {
-      for (let j = 0, len = sets.length; j < len; j++) {
-        const candidateSet = sets[j].concat(arr[i]);
-        const candidateSum = utils.sum(candidateSet);
-        if (candidateSum <= max) {
-          sets.push(candidateSet);
-          sums.push(candidateSum);
-        }
-      }
-    }
-    return sums[utils.random(0, sums.length - 1)];
-  },
-};
+import { mathFunctions} from "./utils";
 
 
-
-
-
-
-
-const StarMatch = () => {
-  const stars=5;
+const App = () => {
+  const stars=mathFunctions.random(1,9);
   return (
     <div className="game">
       <div className="help">
@@ -51,18 +10,10 @@ const StarMatch = () => {
       </div>
       <div className="body">
         <div className="left">
-          {utils.range(1,stars).map((starId)=><div className="star" key={starId}></div>)}
+          {mathFunctions.range(1,stars).map((starId)=><div className="star" key={starId}></div>)}
         </div>
         <div className="right">
-          <button className="number">1</button>
-          <button className="number">2</button>
-          <button className="number">3</button>
-          <button className="number">4</button>
-          <button className="number">5</button>
-          <button className="number">6</button>
-          <button className="number">7</button>
-          <button className="number">8</button>
-          <button className="number">9</button>
+          {mathFunctions.range(1,9).map((number)=><button className="number" key={number}>{number}</button>)}
         </div>
       </div>
       <div className="timer">Time Remaining: 10</div>
@@ -72,7 +23,7 @@ const StarMatch = () => {
 
 
 
-export default StarMatch;
+export default App;
 
 
 

2ffa0857f51038b18e76193d50e36f6234570156
manjari jain <manjarijain98@gmail.com>
2021-08-28 13:57:17 +0530

    ui state

--- src/App.js
+++ src/App.js
@@ -8,6 +8,25 @@ import StarsDisplay from './StarsDisplay';
 
 const App = () => {
   const [stars,setStars]=useState(mathFunctions.random(1,9));
+  const [availableNums,setAvailableNums]=useState(mathFunctions.range(1,9));
+  const [candidateNums,setCandidateNums]=useState([]);
+
+  const candidatesAreWrong=mathFunctions.sum(candidateNums)>stars;
+
+  const numberStatus=(number)=>{
+      if(!availableNums.includes(number))
+      {
+         return "used";
+      }
+
+      if(candidateNums.includes(number))
+      {
+         return candidatesAreWrong ? 'wrong' :'candidate';
+      }
+
+      return "available";
+  }
+
   return (
     <div className="game">
       <div className="help">
@@ -18,7 +37,14 @@ const App = () => {
           <StarsDisplay count={stars}/>
         </div>
         <div className="right">
-          {mathFunctions.range(1,9).map((number)=><PlayNumber key={number} number={number}/>)}
+          {mathFunctions.range(1,9).map(
+            (number)=>
+              <PlayNumber 
+                key={number} 
+                number={number}
+                status={numberStatus(number)}
+              />
+          )}
         </div>
       </div>
       <div className="timer">Time Remaining: 10</div>
--- src/PlayNumber.js
+++ src/PlayNumber.js
@@ -1,9 +1,14 @@
+import {colors} from './utils';
+
 const PlayNumber=(props)=>{
 
-    const {number}=props;
+    const {number,status}=props;
 
     return(
-        <button className="number">
+        <button 
+           className="number" 
+           style={{background:colors[status]}}
+        >
             {number}
         </button>
     );
